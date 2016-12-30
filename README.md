44759dbdc8d1be266700c9c8bf871a61e2c4446d
Patrick Hollweck <patrick-hollweck@gmx.de>
2016-12-30 23:16:06 +0100

    Updated RepairAndWash
    Added support for diffrent actions and a ini file

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# GtaRagePlugins
-A small collection of ragepluginhook.net / lcpdfr.com plugins.
+![HEADER](https://raw.githubusercontent.com/FetzenRndy/GtaRagePlugins/master/git/Header.png)
+
+# GTA RagePluginHook Plugins . By Fetzen
+
+Here you have some open source plugins for the Rage Plugin hook.
+
+#### What is this all about ?
+
+I made some basic Rage plugins and decided to open source them all of my plugins can be found at a compiled version at the official LSPDFR website.
+
+#### Suggestions?
+
+If you have a Suggestion please open a issue ticket, I would be more than happy to make a plugin or a feature you want in the Game a reality.
+
+#### Problems?
+
+Please open a Issue ticket with the logs of the Game, the used version of the Plugin and if you can a picture of your GTA - lspdfr - Rage and Plugins folder.
+
+#### Want to Contribute ? 
+
+I will be more than Happy if you have a Idea or found a bug you have a solution for and want to contribute the code yourself, just some stuff:
+
+What you will need:
+- A legal copy of GTA (For Testing),
+- The Rage Plugin Hook,
+- Probably LSPDFR,
+- C# knowlege,
+- Visual Studio
+
+To Contribute, you can open a Pull request at any time, and please comment and Test your Code. Thanks.
--- RepairAndWash/EntryPoint.cs
+++ RepairAndWash/EntryPoint.cs
@@ -1,6 +1,14 @@
-﻿using Rage;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Rage;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
 
-[assembly: Rage.Attributes.Plugin("RepairAndWash", Description = "Repairs and Washes your Car on KeyPress", Author = "Patrick Hollweck")]
+[assembly:
+	Rage.Attributes.Plugin("RepairAndWash", Description = "Repairs and Washes your Car on KeyPress",
+		Author = "Patrick Hollweck")]
 
 /*
  * Developed by Patrick Hollweck - GitHub: https://github.com/FetzenRndy;
@@ -11,62 +19,185 @@ namespace RepairAndWash
 {
 	public class EntryPoint
 	{
+		/// <summary>
+		/// Main Entry Point of the Plugin
+		/// </summary>
 		public static void Main()
 		{
-			WaitForActivation();
+			IniHandler();
 
-			Game.DisplayNotification("Repair and Wash - Loaded!");
-			Game.LogTrivial("Repair and Wash - Loaded!");
+			Game.DisplayNotification("~b~RepairAndWash ~w~ Has been loaded ~g~Successfully.");
+			StartPlugin();
+		}
+
+		private static Keys KeyCleanAndRepair;
+		private static Keys KeyClean;
+		private static Keys KeyRepair;
+
+
+		/// <summary>
+		/// Handle the input from the INI file
+		/// </summary>
+		public static void IniHandler()
+		{
+			InIFile iniHandler = new InIFile();
+
+			iniHandler.path = "Plugins/RepairAndWash.ini";
+
+			if (!File.Exists(iniHandler.path))
+			{
+				iniHandler.WriteValue("KEYBINDINGS", "HELP",
+					"For Help with the ini file visit : https://msdn.microsoft.com/de-de/library/system.windows.forms.keys(v=vs.110).aspx");
+				iniHandler.WriteValue("KEYBINDINGS", "CleaningAndRepairing", "NumPad0");
+				iniHandler.WriteValue("KEYBINDINGS", "Cleaning", "NumPad1");
+				iniHandler.WriteValue("KEYBINDINGS", "Repairing", "NumPad2");
+			}
+			else
+			{
+				KeysConverter kc = new KeysConverter();
+
+				try
+				{
+					object convertFromString = kc.ConvertFromString(iniHandler.ReadValue("KEYBINDINGS", "CleaningAndRepairing"));
+					if (convertFromString != null)
+					{
+						KeyCleanAndRepair = (Keys) convertFromString;
+					}
+
+					object fromString = kc.ConvertFromString(iniHandler.ReadValue("KEYBINDINGS", "Cleaning"));
+					if (fromString != null)
+					{
+						KeyClean = (Keys) fromString;
+					}
+
+					object s = kc.ConvertFromString(iniHandler.ReadValue("KEYBINDINGS", "Repairing"));
+					if (s != null)
+					{
+						KeyRepair = (Keys) s;
+					}
+				}
+				catch (Exception)
+				{
+					Game.DisplaySubtitle("~r~ERROR WHILE TRYING TO PARSE THE INI FILE OF REPAIR AND WASH PLEASE FIX IT!");
+				}
+			}
 		}
 
 		/// <summary>
 		/// Sets up a new GameFibre and wait for Activation of the Key. Then call the OnActivation function
 		/// </summary>
-		public static void WaitForActivation()
+		public static void StartPlugin()
 		{
-			Game.LogTrivial("Repair and Wash - Waiting for Activation");
+			Game.LogTrivial("Waiting for Activation");
 			GameFiber.StartNew(delegate
 			{
-				while (true)
-				{
-					GameFiber.Yield();
-
-					if (Game.IsKeyDown(System.Windows.Forms.Keys.NumPad0))
-					{
-						break;
-					}
-
-					Game.LogTrivial("Repair and Wash KEYDOWN!");
-				}
-
-				OnActivation();
+				WaitForKeydown();
+				Game.LogTrivial("Loaded! - log");
 			});
-
-			GameFiber.Hibernate();
 		}
 
 		/// <summary>
-		/// Get player and their vehicle, If Player is inside a Vehicle clean and repair it, Then start a new GameFibre.
+		/// Wait until key is Pressed.
 		/// </summary>
-		public static void OnActivation()
+		public static void WaitForKeydown()
+		{
+			while (true)
+			{
+				GameFiber.Yield();
+
+				if (Game.IsKeyDown(KeyCleanAndRepair))
+				{
+					Game.LogTrivial("KeyCleanAndRepair - KEYDOWN!");
+					OnKeyDown("RepairAndClean");
+					break;
+				}
+
+				if (Game.IsKeyDown(KeyClean))
+				{
+					Game.LogTrivial("KeyClean - KEYDOWN!");
+					OnKeyDown("Clean");
+					break;
+				}
+
+				if (Game.IsKeyDown(KeyRepair))
+				{
+					Game.LogTrivial("KeyRepair - KEYDOWN!");
+					OnKeyDown("Repair");
+					break;
+				}
+			}
+		}
+
+		/// <summary>
+		/// According to the user wished action eighter clean, repair or Clean and Repair the Car and Wait for another keydown;
+		/// </summary>
+		public static void OnKeyDown(string Action)
 		{
 			Ped player = Game.LocalPlayer.Character;
 			Vehicle playerVehicle = player.CurrentVehicle;
 
 			if (playerVehicle != null)
 			{
-				playerVehicle.Repair();
-				playerVehicle.Wash();
+				if (Action == "RepairAndClean")
+				{
+					playerVehicle.Repair();
+					playerVehicle.Wash();
 
-				Game.LogTrivial("Repair and Wash - Repaired and washed");
-				Game.DisplayNotification("Repaired and cleaned your car!");
+					Game.LogTrivial("Repaired and cleaned");
+					Game.DisplayNotification("~g~Repaired and cleaned your car!");
+				}
+				else if (Action == "Repair")
+				{
+					playerVehicle.Repair();
+
+					Game.LogTrivial("Repaired");
+					Game.DisplayNotification("~g~Repaired your car!");
+				}
+				else if (Action == "Clean")
+				{
+					playerVehicle.Wash();
+
+					Game.LogTrivial("Cleaned");
+					Game.DisplayNotification("~g~Cleaned your car!");
+				}
 			}
 			else
 			{
-				Game.DisplayNotification("You are not in a Vehicle...");
+				Game.DisplayNotification("~r~You are not in a Vehicle...");
 			}
 
-			WaitForActivation();
+			WaitForKeydown();
+		}
+
+		/// <summary>
+		/// INI Files handling class
+		/// </summary>
+		class InIFile
+		{
+			public string path;
+
+			[DllImport("kernel32")]
+			private static extern long WritePrivateProfileString(string section,
+			string key, string val, string filePath);
+
+			[DllImport("kernel32")]
+			private static extern int GetPrivateProfileString(string section,
+			string key, string def, StringBuilder retVal,
+			int size, string filePath);
+
+			public void WriteValue(string Section, string Key, string Value)
+			{
+				WritePrivateProfileString(Section, Key, Value, this.path);
+			}
+
+			public string ReadValue(string Section, string Key)
+			{
+				StringBuilder temp = new StringBuilder(255);
+				int i = GetPrivateProfileString(Section, Key, "", temp,
+												255, this.path);
+				return temp.ToString();
+
+			}
 		}
 	}
 }
