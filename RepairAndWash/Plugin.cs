6a2b4643715418c017158526d67d321f1a8a9a98
Patrick Hollweck <patrick-hollweck@gmx.de>
2018-10-27 19:51:00 +0200

    Remove audio

--- RepairAndWash/Plugin.cs
+++ RepairAndWash/Plugin.cs
@@ -15,121 +15,119 @@ using Rage.Attributes;
 
 namespace RepairAndWash
 {
-	using System;
+    using System;
 
-	// Rage exposes a Rage.Task class. Which creates a name clash.
-	using DotNetTask = System.Threading.Tasks.Task;
+    // Rage exposes a Rage.Task class. Which creates a name clash.
+    using DotNetTask = System.Threading.Tasks.Task;
 
-	using Rage;
+    using Rage;
 
-	using RepairAndWash.Core.Common;
-	using RepairAndWash.Core.Components;
-	using RepairAndWash.Core.Components.ErrorReporting.Reporters;
+    using RepairAndWash.Core.Common;
+    using RepairAndWash.Core.Components;
+    using RepairAndWash.Core.Components.ErrorReporting;
+    using RepairAndWash.Core.Components.ErrorReporting.Reporters;
 
-	/// <summary>
-	/// Main logic and Entry Point of the Plugin
-	/// </summary>
-	public static class Plugin
-	{
-		/// <summary>
-		/// Main Entry Point of the Plugin
-		/// </summary>
-		[STAThread]
-		public static void Main()
-		{
-			ErrorHandler.RegisterReporter(new IngameMessageReporter());
+    /// <summary>
+    /// Main logic and Entry Point of the Plugin
+    /// </summary>
+    public static class Plugin
+    {
+        /// <summary>
+        /// Main Entry Point of the Plugin
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                ErrorHandler.RegisterReporter(new IngameMessageReporter());
 
-			Audio.Setup();
-			StartPlugin();
+                StartPlugin();
+                Util.DisplayNotification($"~b~RepairAndWash ~w~V {Global.Application.CurrentVersion} has been loaded ~g~Successfully~w");
 
-			Game.DisplayNotification("~b~RepairAndWash ~w~V" + Global.Application.CurrentVersion + " Has been loaded ~g~Successfully~w.");
+                DotNetTask.Run(async () => { await Updater.CheckUpdate(); });
+            }
+            catch (Exception exception)
+            {
+                ErrorHandler.Report("Something went wrong :(", exception);
+            }
+        }
 
-			DotNetTask.Run(async () =>
-			{
-				await Updater.CheckUpdate();
-			});
-		}
+        /// <summary>
+        /// Sets up a new GameFibre and Enteres the Main Programm flow
+        /// </summary>
+        private static void StartPlugin()
+        {
+            GameFiber.StartNew(() => WaitForKeydown());
+        }
 
-		/// <summary>
-		/// Sets up a new GameFibre and Enteres the Main Programm flow
-		/// </summary>
-		private static void StartPlugin()
-		{
-			GameFiber.StartNew(
-				delegate { WaitForKeydown(); }
-			);
-		}
+        /// <summary>Executes a action based on KeyPress</summary>
+        /// <param name="action">The action.</param>
+        private static void OnKeyDown(Global.Enums.Action action)
+        {
+            // Gets the LocalPlayer and the CurrentVehicle if he is in one
+            Ped player = Game.LocalPlayer.Character;
+            Vehicle playerVehicle = player.CurrentVehicle;
 
-		/// <summary>Executes a action based on KeyPress</summary>
-		/// <param name="action">The action.</param>
-		private static void OnKeyDown(Global.Enums.Action action)
-		{
-			// Gets the LocalPlayer and the CurrentVehicle if he is in one
-			Ped player = Game.LocalPlayer.Character;
-			Vehicle playerVehicle = player.CurrentVehicle;
+            // If the PlayersVehicle is null he is not in a Vehicle -> Can't be Repaired
+            if (playerVehicle == null)
+            {
+                Util.DisplayNotification("~r~You are not in a Vehicle...");
+                return;
+            }
 
-			// If the PlayersVehicle is null he is not in a Vehicle -> Can't be Repaired
-			if (playerVehicle == null)
-			{
-				Util.DisplayNotificationType("~r~You are not in a Vehicle...");
-				return;
-			}
+            // String Containing the Type of the Vehicle obtained by Calling a Helper Methode
+            string playerVehicleType = Util.GetVehicleType(playerVehicle);
 
-			// String Containing the Type of the Vehicle obtained by Calling a Helper Methode
-			string playerVehicleType = Util.GetVehicleType(playerVehicle);
+            // Execute a Function based on the Action Param
+            if (action == Global.Enums.Action.RepairAndWash)
+            {
+                // Repaires the PlayerVehicle
+                playerVehicle.Repair();
+                // Washes the PlayerVehicle
+                playerVehicle.Wash();
 
-			// Execute a Function based on the Action Param
-			if (action == Global.Enums.Action.RepairAndWash)
-			{
-				// Repaires the PlayerVehicle
-				playerVehicle.Repair();
-				// Washes the PlayerVehicle
-				playerVehicle.Wash();
+                // Play a Sound and Display a Notification if not Disabled by the User
+                Util.DisplayNotification("~g~Repaired and cleaned your " + playerVehicleType + "!");
+            }
+            else if (action == Global.Enums.Action.Repair)
+            {
+                playerVehicle.Repair();
 
-				// Play a Sound and Display a Notification if not Disabled by the User
-				Audio.PlaySound(Global.Sound.Sounds.RepairAndWash);
-				Util.DisplayNotificationType("~g~Repaired and cleaned your " + playerVehicleType + "!");
-			}
-			else if (action == Global.Enums.Action.Repair)
-			{
-				playerVehicle.Repair();
+                Util.DisplayNotification("~g~Repaired your vehicle " + playerVehicleType + "!");
+            }
+            else if (action == Global.Enums.Action.Wash)
+            {
+                playerVehicle.Wash();
 
-				Audio.PlaySound(Global.Sound.Sounds.Repair);
-				Util.DisplayNotificationType("~g~Repaired your vehicle " + playerVehicleType + "!");
-			}
-			else if (action == Global.Enums.Action.Wash)
-			{
-				playerVehicle.Wash();
+                Util.DisplayNotification("~g~Cleaned your vehicle " + playerVehicleType + "!");
+            }
+        }
 
-				Audio.PlaySound(Global.Sound.Sounds.Wash);
-				Util.DisplayNotificationType("~g~Cleaned your vehicle " + playerVehicleType + "!");
-			}
-		}
+        /// <summary>
+        /// Wait until key is Pressed.
+        /// </summary>
+        private static void WaitForKeydown()
+        {
+            while (true)
+            {
+                GameFiber.Yield();
 
-		/// <summary>
-		/// Wait until key is Pressed.
-		/// </summary>
-		private static void WaitForKeydown()
-		{
-			while (true)
-			{
-				GameFiber.Yield();
+                if (Game.IsKeyDown(Global.Controls.RepairAndWash))
+                {
+                    OnKeyDown(Global.Enums.Action.RepairAndWash);
+                }
 
-				if (Game.IsKeyDown(Global.Controls.RepairAndWash))
-				{
-					OnKeyDown(Global.Enums.Action.RepairAndWash);
-				}
+                if (Game.IsKeyDown(Global.Controls.Wash))
+                {
+                    OnKeyDown(Global.Enums.Action.Wash);
+                }
 
-				if (Game.IsKeyDown(Global.Controls.Wash))
-				{
-					OnKeyDown(Global.Enums.Action.Wash);
-				}
-
-				if (Game.IsKeyDown(Global.Controls.Repair))
-				{
-					OnKeyDown(Global.Enums.Action.Repair);
-				}
-			}
-		}
-	}
+                if (Game.IsKeyDown(Global.Controls.Repair))
+                {
+                    OnKeyDown(Global.Enums.Action.Repair);
+                }
+            }
+        }
+    }
 }
